8c7cf8839d1e0b09adc76d7b0610bc78f010a6f3
Girish Amudala <amudalagirish@gmail.com>
2024-02-12 11:56:10 +0530

    added core app

---
+++ MarketMingle/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    # Define your Order model fields
+    pass
